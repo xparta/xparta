f1c37a7deb819a75b8d04ab365bfa8b6e674db62
xparta <tomarobaski@gmail.com>
2016-01-18 08:59:40 +0000

    second commit-added bootstrap and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+ 
+  get "about" => "pages#about"
+  get "contact" => "pages#contact"
+  get "portfolio" => "pages#portfolio"
+  root 'pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
